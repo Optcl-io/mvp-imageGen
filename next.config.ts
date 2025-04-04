733141a5b9cf91fc59fb581ebeb91cf01db4df3d
MukundS <mukundsoni08@gmail.com>
2025-04-04 17:44:01 +0530

    fix image url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com","oaidalleapiprodscus.blob.core.windows.net"],
   }
 };
 
